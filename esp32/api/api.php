8af6a7db0cbda2e42b027b203d5adb1c322bf180
Febri Nur Diansah <82320889+febrinurdiansah@users.noreply.github.com>
2024-04-30 22:44:07 +0700

    Update api.php

--- esp32/api/api.php
+++ esp32/api/api.php
@@ -1,7 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"); 
 error_reporting(E_ERROR | E_PARSE);
+
 $host = "localhost";
-$user = "root";
+$user = "";
 $pass = "";
 $db = "esp32";
 
